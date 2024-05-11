d3bf90e427a51c15bde35c11062f2a9cc8b97296
Happi <153699187+Happi143@users.noreply.github.com>
2024-05-11 20:53:51 +0800

    Add files via upload

---
+++ document.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Certification</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+        }
+        h1 {
+            text-align: center;
+            margin-top: 20px;
+            color: black;
+            font-family: 'Times New Roman', Times, serif;
+            word-spacing: 15px;
+        }
+        .header {
+            display: flex;
+            align-items: center;
+            margin-left: 9em;
+        }
+        .header img {
+            max-width: 100px; /* Adjust the size of the logo as needed */
+            margin-right: 10px;
+        }
+        h4 {
+            text-align: left;
+            margin: 0;
+            font-size: 17px;
+            font-weight: 300;
+            font-family: Arial, Helvetica, sans-serif;
+        }
+        .table-container {
+            width: 80%;
+            margin: 20px auto; /* Center the div horizontally */
+        }
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.1);
+            border-radius: 10px;
+            overflow: hidden;
+        }
+        th, td {
+            padding: 5px;
+            text-align: left;
+            border-bottom: 1px solid black;
+        }
+        th {
+            background-color: #79ab79;
+            color: #333;
+            font-weight: bold;
+            text-align: center;
+        }
+        td {
+            font-weight: normal;
+        }
+        tr:hover {
+            background-color: #f4f4f4;
+        }
+        .separator {
+            border-left: 1px solid black;
+        }
+        .university-name {
+            color: maroon;
+            font-size: 20px;
+            font-weight: bold;
+            font-family: 'Times New Roman', Times, serif;
+        }
+        .school {
+            color: black;
+            font-size: 19px;
+            font-family: 'Times New Roman', Times, serif;
+        }
+        .semester {
+            text-align: center;
+            margin-top: -19px;
+        }
+        .paragraph {
+         text-align: justify;
+         margin: 109px auto;
+         max-width: 676px;
+                    }
+        .paragraph1{
+            text-align: justify;
+            margin: 90px auto;
+            margin-top: 90px;
+            max-width: 676px;
+            margin-top: -4em;
+                    }
+        .prepared{
+         text-align:center;
+         margin: 6em;
+        }
+        .chairperson{
+            text-align: center;
+            font-weight: bold;
+            margin: -2em;
+        }
+        .track{
+            text-align: center;
+            margin: 2em;
+        }
+        .approved{
+            text-align:center;
+            margin: 3em;
+        }
+        .director{
+            text-align: center;
+             font-weight: bold;
+             margin: 0em;
+        }
+        .position{
+            text-align: center;
+            margin: 0;
+        }
+        
+    
+    </style>
+</head>
+<body>
+    <div class="header">
+        <img src="images/MSU copy.png" alt="Logo"> <!-- Change "logo.png" to the path of your logo -->
+        <img src="images/MarawiSeniorHigh-removebg.png" alt="Logo"> <!-- Change "logo.png" to the path of your logo -->
+        <div>
+            <h4>Republic Of The Philippines</h4>
+            <h4 class="university-name">MINDANAO STATE UNIVERSITY</h4>
+            <h4 class="school">SENIOR HIGH SCHOOL</h4>
+            <h4>Marawi City</h4>
+        </div>
+    </div>
+    <hr style="border-color:black;border:1px solid gold;"></hr>
+
+    <h1>CERTIFICATION</h1>
+
+    <p class="paragraph"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp This is to certify that based on the records available in this school,<strong> Mr. Ammar
+M. Ampuan</strong>, with LRN 478017150966, is officially
+enrolled as a<strong> Grade 12 student</strong> under the <strong>Technical-Vocational-Livelihood Track –
+Information and Communications Technology (ICT) strand</strong> this first semester,
+academic year 2023 – 2024.</p>
+
+<p class="paragraph1">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp This certification is issued on January 12, 2024 upon the request of Mr. Ampuan in support of his registration for a scholarship.</p>
+
+
+<p class="prepared">Prepared by</p>
+
+<p class="chairperson">Ms. Haniah Krys E. Illupa</p>
+<p class="track">TVL Track Chairperson</p>
+
+<p class="approved">Approved by</p>
+
+<p class="director">Junaina M. Dimalna, MAELT</p>  
+<p class="position">Director</p> 
+
+    
+    
